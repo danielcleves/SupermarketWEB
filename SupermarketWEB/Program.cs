3519e20d7a85d31bf7b2383ae90275f0cb7863a1
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-29 20:01:46 -0500

    Initial commit

---
+++ SupermarketWEB/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+
+namespace SupermarketWEB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<SupermarketContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
