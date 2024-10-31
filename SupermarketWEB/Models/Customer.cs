140c152f63f95247ffb0b0cb88cc2c69f56979f3
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-30 23:16:17 -0500

    ✨ feat: Add models
    for Customer and PayMode

---
+++ SupermarketWEB/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string DocumentNumber { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string? Address { get; set; }
+		public DateTime? Birthday { get; set; }
+		public string? PhoneNumber { get; set; }
+		public string? Email { get; set; }
+	}
+}
